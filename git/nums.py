b70c718963268ef9d276d388e27b2a0118521a39
Vadim-Valiullin <vadimvr-ufa@yandex.ru>
2022-09-14 16:24:53 +0500

    Добавлена функция multi

--- git/nums.py
+++ git/nums.py
@@ -1,10 +1,15 @@
-def plus(a,b):
-    return a+b
-text = "new text"
+def plus(a, b):
+    return a + b
+
+
 def minus(a, b):
     return a - b
+
+def multi(a, b):
+    return a * b
 
 
 if __name__ == '__main__':
     print(plus(2, 2))
     print(minus(4, 2))
+    print(multi(3, 3))
